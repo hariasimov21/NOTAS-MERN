2970cde51eb71148536e3cd6b5c1769a09dde5b5
jaime <james18bt@gmail.com>
2021-04-12 13:12:22 -0400

    componente de crar usuario actualizado

--- curso-react/src/components/Navigation.js
+++ curso-react/src/components/Navigation.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
-import { Nav } from 'react-bootstrap'; 
-import '../css/styles.css';
-
+import React, { Component } from "react";
+import { Nav } from "react-bootstrap";
+import "../css/styles.css";
 
 export default class Navigation extends Component {
-    render() {
-        return (
-            <Nav
-            className="navbar navbar-expand-lg navbar-dark bg-dark"
- // activeKey="/home"
-  // onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
->
-  <Nav.Item className ="navHeader">
-    <Nav.Link href="/">App de Notas</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link href="/create">Crear Nota</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link href="/user">Crear Usuario</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link href="/edit/:id">crear nota</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>
-        )
-    }
+  render() {
+    return (
+      <Nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark ml-auto justify-content-end"
+        // activeKey="/home"
+        // onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item className="navHeader">
+          <Nav.Link className="nav-link" href="/">
+            Listado de Notas
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="navHeader">
+          <Nav.Link className="nav-link" href="/create">
+            Crear Nota
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="navHeader">
+          <Nav.Link className="nav-link" href="/user">
+            Crear Usuario
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    );
+  }
 }
